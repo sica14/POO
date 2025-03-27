a909e014f44fd58e7e345386870b7a9c2f8f8186
sica14 <142255911+sica14@users.noreply.github.com>
2025-03-27 09:16:25 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,26 @@
 import java.util.Scanner;
-import formas.Retangulo;
+
+import grafico.Circulo;
+import grafico.Retangulo;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Digite o X e Y: ");
         Retangulo r1 = new Retangulo(scanner.nextInt(), scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
-        System.out.println(r1);
-        Retangulo r2 = new Retangulo(3,4,5,6);
         r1.mover(4,5);
-        Retangulo r3 = new Retangulo(8,7,9,10);
-        r3.desenhar();
-        Retangulo r4 = new Retangulo(3,4,5,6);
-        r4.redimensionar(3,4);
-        Retangulo r5 = new Retangulo(3,4,5,6);
-        r5.dividir(3,5);
-        r2.desenhar();
-        r3.desenhar();
-        r4.desenhar();
-        r5.desenhar();
-      }
+        r1.desenhar();
+        r1.dividir(6,8);
+        r1.redimensionar(3,4);
+        System.out.println(r1);
+
+
+        Circulo c1 = new Circulo(scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
+        c1.mover(4,5);
+        c1.desenhar();
+        c1.redimensionar(3);
+        c1.dividir(7,8,9);
+        System.out.println(c1);
+
+    }
 }
